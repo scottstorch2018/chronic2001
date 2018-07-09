ce8f60b1345ff43f619e54590e4967a424991c41
Mounir Boutlane <mounir.boutlane@adp.com>
2018-07-09 16:30:31 +0200

    mise a jour de la branch watcher

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -6,7 +6,7 @@ public class HelloWorld2 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World 2 amigo !!!");
+        System.out.println("Hello, World 2 amigo que je mets à jour !!!");
     }
 
 }
